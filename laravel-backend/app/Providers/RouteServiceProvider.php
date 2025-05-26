d0e3fd23c913046d7a8db671b81d6c246131c32b
JiaSeng02 <jiaseng02@gmail.com>
2025-05-26 23:49:10 +0800

    Initial commit

---
+++ laravel-backend/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        // API routes
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+
+        // Web routes
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    }
+}
