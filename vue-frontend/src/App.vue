7303f6861a3d7ad0535920b276c3baa9dbea809e
JiaSeng02 <jiaseng02@gmail.com>
2025-05-26 23:56:20 +0800

    Converted vue-frontend from submodule to directory

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Car Listings</h1>
+    <div v-for="car in cars" :key="car.id">
+      {{ car.brand }} {{ car.model }} - ${{ car.price }} ({{ car.year }})
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      cars: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8000/api/cars")
+      .then((response) => {
+        this.cars = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
+</script>
